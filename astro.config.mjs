0d382c05ee6c61555003946c2106ed2bcfa553f8
Miso <hisokamiso2001@gmail.com>
2026-08-14 17:32:11 +0530

    Add Google and Bing verification

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,23 @@ export default defineConfig({
     starlight({
       title: 'Sling TV Login',
 
+      head: [
+        {
+          tag: 'meta',
+          attrs: {
+            name: 'google-site-verification',
+            content: 'bpyG_46Oxb8tkddkShnQUSNXXlLsS3_uprz6BW6X85g',
+          },
+        },
+        {
+          tag: 'meta',
+          attrs: {
+            name: 'msvalidate.01',
+            content: 'B807FE4A03DB636D50EF7962DDBBE11F',
+          },
+        },
+      ],
+
       sidebar: [
         {
           label: 'Sling TV Login',
